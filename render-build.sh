5bd8c7af59e68d17361fbf4b50d0d5cf593c468c
johnsteven9988776655 <johnsteven9988776655>
2025-11-19 15:54:33 +0300

    updates

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y ffmpeg
+set -x
+
+# Download static ffmpeg build
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
+tar -xf ffmpeg.tar.xz
+mv ffmpeg-*-amd64-static/ffmpeg /usr/local/bin/
+mv ffmpeg-*-amd64-static/ffprobe /usr/local/bin/
+chmod +x /usr/local/bin/ffmpeg
+chmod +x /usr/local/bin/ffprobe
+
+# Install python dependencies
 pip install -r requirements.txt
